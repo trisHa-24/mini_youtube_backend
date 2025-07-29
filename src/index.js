9d8a0cf3a6de6a777993a0683c10a8bd5e85be55
trisHa-24 <mt123456trisha@gmail.com>
2025-07-29 12:01:05 +0530

    error response utils

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ dotenv.config({
     path: "./.env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+   const server = app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+    server.on("error", (error) => {
+        console.log("Error while starting server", error);
+        throw error;
+    });    app.get('/user', asyncHandler(getUser));
+})
+.catch((error) => {
+    console.log("Error connecting to MongoDB:", error);   
+})
